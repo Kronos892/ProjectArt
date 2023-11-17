5d892560b5962ab5b77f86c7d34156da6e9c5566
Kronos892 <kuba.berezowski123@gmail.com>
2023-11-17 22:25:19 +0100

    Add new files

--- components/navbar.js
+++ components/navbar.js
@@ -3,13 +3,13 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { FontAwesome5, FontAwesome } from '@expo/vector-icons'
 
 
-
+const iconSize = 30;
 const Navbar = () => {
     return (
         <View style={style.View}>
-            <FontAwesome name="home" color={'#fff'} size={40} />
-            <FontAwesome name="search" color={'#fff'} size={40} />
-            <FontAwesome name="heart" color={'#fff'} size={40} />
+            <FontAwesome name="home" color={'#fff'} size={iconSize} />
+            <FontAwesome name="search" color={'#fff'} size={iconSize} />
+            <FontAwesome name="heart" color={'#fff'} size={iconSize} />
         </View>
     )
 }
@@ -20,7 +20,7 @@ const style = StyleSheet.create({
         paddingTop: 50,
     },
     View: {
-        height: 80,
+        
         flexDirection: "row",
         justifyContent: "space-around",
         padding: 15,
